79d37a061acfb37f4ab5af826bd1a7d7ce39b0ab
Adebola Odufuwa <113560716+adebola-duf@users.noreply.github.com>
2023-09-11 20:16:31 +0100

    Included the Authorization to update using the update bot.

--- update_bot.py
+++ update_bot.py
@@ -9,8 +9,9 @@ token = os.getenv("UPDATE_BOT_TOKEN")
 db = os.getenv("DB_NAME")
 db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_INTERNAL_HOST")
+db_host = os.getenv("DB_EXTERNAL_HOST")
 db_port = os.getenv("DB_PORT")
+admin_password = os.getenv("ADMIN_PASSWORD")
 
 connection_params = {"database": db,
                      "user": db_username,
@@ -20,6 +21,8 @@ connection_params = {"database": db,
 
 bot = telebot.TeleBot(token, parse_mode=None)
 userglobal_id = 1
+password = None
+updated_wallets = []
 
 
 def gen_markup():
@@ -34,14 +37,39 @@ def gen_markup():
 def start(message):
     bot.reply_to(message, "Hi Adebola. I hope you are doing great.")
     bot.send_message(
-        message.chat.id, "To update the wallet balance of any user click /enter_user_id")
+        message.chat.id, "To continue, click on /password")
+    # bot.send_message(
+    #     message.chat.id, "To update the wallet balance of any user click /enter_user_id")
+
+
+@bot.message_handler(commands=['password'])
+def password(message):
+    msg = bot.send_message(message.chat.id, "Enter your password Adebola")
+    bot.register_next_step_handler(msg, password_enter)
+
+
+def password_enter(message):
+    global password
+    password = message.text
+    if password == admin_password:
+        bot.reply_to(message,
+                     text=f"Authorised ✅. You can now go on to do other stuffs.")
+        bot.delete_message(message.chat.id, message.message_id)
+    else:
+        bot.reply_to(
+            message, "Authentication Failed. click on /password for another chance. Don't worry you have unlimited trials and trust me you won't get it 🤗")
 
 
 @bot.message_handler(commands=['enter_user_id'])
 def handle_enter_name(message):
-    msg = bot.reply_to(message, "Please enter the user's id:")
+    global password
+    if password == admin_password:
+        msg = bot.reply_to(message, "Please enter the user's id:")
 
-    bot.register_next_step_handler(msg, process_user_id)
+        bot.register_next_step_handler(msg, process_user_id)
+    else:
+        bot.reply_to(
+            message, "You are not authorised. Click /password for authentication.")
 
 
 def process_user_id(message):
@@ -56,10 +84,10 @@ def process_user_id(message):
     result = cursor.fetchone()
 
     if result:  # to verify if i actually inputed a correct user_id
-        bot.send_message(
-            message.chat.id, f"user id {userglobal_id}is valid. Do you want to:", reply_markup=gen_markup())
+        bot.reply_to(
+            message, f"user id {userglobal_id} is valid. Do you want to:", reply_markup=gen_markup())
     else:
-        bot.send_message(
+        bot.reply_to(
             message.chat.id, f"user_id {userglobal_id} doesn't exist.")
 
     cursor.close()
@@ -96,7 +124,8 @@ def add_to_wallet(message):
     cursor.close()
     connection.close()
     bot.send_message(
-        message.chat.id, f"You have added ₦{amount} to {userglobal_id}'s wallet.Click /done if you are done updating {userglobal_id}'s wallet.")
+        message.chat.id, f"You have added ₦{amount} to {userglobal_id}'s wallet. Click /done if you are done for today.")
+    updated_wallets.append(str(userglobal_id))
 
 
 def deduct_from_wallet(message):
@@ -112,15 +141,22 @@ def deduct_from_wallet(message):
     connection.close()
 
     bot.send_message(
-        message.chat.id, f"You have deducted ₦{amount} from {userglobal_id}'s wallet. Click /done if you are done updating {userglobal_id}'s wallet.")
+        message.chat.id, f"You have deducted ₦{amount} from {userglobal_id}'s wallet. Click /done if you are done for today.")
+    updated_wallets.append(str(userglobal_id))
 
 
 @bot.message_handler(commands=['done'])
 def done(message):
-    global userglobal_id
-    bot.reply_to(
-        message, f"You have update {userglobal_id}'s wallet. Keep the grind up Adebola. Goal=🦄")
-    userglobal_id = 1
+    global userglobal_id, password
+    if password == admin_password:
+        bot.reply_to(
+            message, f"You have updated user id {', '.join(set(updated_wallets))} wallets. Keep the grind up Adebola. Goal=🦄")
+        userglobal_id = 1
+        password = None
+        updated_wallets = []
+    else:
+        bot.reply_to(
+            message, "You are not authorised. Click /password for authentication.")
 
 
 bot.polling()
