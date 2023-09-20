bbbc9ea9f4fd83492cf216b9a00fc1e2e45e3da1
Adebola Odufuwa <113560716+adebola-duf@users.noreply.github.com>
2023-09-20 02:02:52 +0100

    Corrected a typo

--- TextPay.py
+++ TextPay.py
@@ -19,7 +19,7 @@ token = os.getenv("TEXT_PAY_BOT_TOKEN")
 db = os.getenv("DB_NAME")
 db_username = os.getenv("DB_USERNAME")
 db_password = os.getenv("DB_PASSWORD")
-db_host = os.getenv("DB_EXTERNAL_HOST")
+db_host = os.getenv("DB_INTERNAL_HOST")
 db_port = os.getenv("DB_PORT")
 
 bot = telebot.TeleBot(token, parse_mode=None)
