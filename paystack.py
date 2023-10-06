1c5f78d404424320c4ba14591fd35fd44680a5fc
Adebola Odufuwa <113560716+adebola-duf@users.noreply.github.com>
2023-10-06 18:25:09 +0100

    created a script for handling paystack webhook events.

---
+++ paystack.py
@@ -0,0 +1,95 @@
+from fastapi import BackgroundTasks, FastAPI
+import hmac
+import hashlib
+from fastapi import FastAPI, HTTPException, Request, status, BackgroundTasks
+from starlette.responses import JSONResponse
+import os
+from dotenv import load_dotenv
+import requests
+
+app = FastAPI()
+secret = bytes("sk_test_af5fa201ddde9006fa054fc5f4e096297cba67b4",
+               'UTF-8')
+
+load_dotenv(".env")
+db = os.getenv("DB_NAME")
+db_username = os.getenv("DB_USERNAME")
+db_password = os.getenv("DB_PASSWORD")
+db_host = os.getenv("DB_INTERNAL_HOST")
+db_port = os.getenv("DB_PORT")
+token = os.getenv("ADMIN_PASSWORD")
+
+connection_params = {"database": db,
+                     "user": db_username,
+                     "host": db_host,
+                     "password": db_password,
+                     "port": db_port}
+
+
+app = FastAPI()
+
+
+def handle_webhook_stuff_on_my_end(event):
+    if event["event"] == "charge.success":
+        # handle the case where the user puts in a non convertible to integer stuff instead of their user_id
+        enterd_user_id = int(
+            event["data"]["metadata"]["custom_fields"][0]["value"])
+        amount_without_paystack_charge = event["data"]["amount"] / 100
+        first_name = event["data"]["customer"]["first_name"]
+        last_name = event["data"]["customer"]["last_name"]
+        paystack_charge = event["data"]["fees"] / 100
+        amount_with_paystack_charge = amount_without_paystack_charge - paystack_charge
+
+        message = f"You have just added ₦{amount_without_paystack_charge} into your wallet, ₦{paystack_charge} was deducted as charges. So you paid ₦{amount_with_paystack_charge} into your wallet. Thanks for texting with us 👍😉."
+        data = {
+            "authentication_token": os.getenv('ADMIN_PASSWORD'),
+            "user_id": enterd_user_id,
+            "message": message,
+            "amount": str(amount_with_paystack_charge),
+            "operation": "add_to_wallet"  # could be anything
+        }
+
+        response = requests.post(
+            f"https://textpay.onrender.com/send-notification-to-user", json=data)
+        if response.status_code == 404:
+            print(response.json()["detail"])
+        elif response.status_code == 401:
+            print(response.json()["detail"])
+        elif response.status_code == 200:
+            print(
+                f"successful payment of ₦{amount_without_paystack_charge} by {first_name} {last_name}, user id: {enterd_user_id}. A fee of ₦{paystack_charge} was deducted. So you have just paid ₦{amount_with_paystack_charge}")
+            print(response.content.decode("utf-8"))
+
+    # i don't think paystack sends events when the transation fails so imma comment this out
+    # else:
+    #     print("failed payment.")
+
+
+@app.post(path="/paystack-webhook")
+async def receive_webhook(request: Request, background_tasks: BackgroundTasks):
+    try:
+        body = await request.body()
+        signature = request.headers['x-paystack-signature']
+
+        hash = hmac.new(secret, body, hashlib.sha512).hexdigest()
+
+        if hash == signature:
+            event = await request.json()
+            background_tasks.add_task(handle_webhook_stuff_on_my_end, event)
+            response_content = {"message": "Webhook received and acknowledged"}
+            return JSONResponse(content=response_content, status_code=200)
+
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid webhoook signature.")
+
+    except Exception as e:
+        if isinstance(e, HTTPException):
+            pass
+        # Handle other types of exceptions
+        print(f"An exception occurred: {e}")
+
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0")
